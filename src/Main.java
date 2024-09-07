267ecf66699038226f13ae68a4dd8321b83c8e3c
Ado Al-Ali <ado14ali@Ados-MBP.lan>
2024-09-07 17:42:40 -0400

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Java");
-        System.err.println("Welcome to computer science");
-        System.err.println("Programming is fun"); 
+        System.out.println("Welcome to computer science");
+        System.out.println("Programming is fun"); 
       }
     }
